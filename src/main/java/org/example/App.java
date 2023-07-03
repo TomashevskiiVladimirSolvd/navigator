08c3604dadadd2dd0879f4650f9baed9ccbbae24
chiemeliecode <ezeokeke.c@northeastern.edu>
2023-07-03 04:42:23 -0400

    Connection pool working MyBatis not working yet

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.ibatis.session.SqlSession;
+import org.example.configurations.ConnectionPool;
+import org.example.myBatis.MyBatisInitializer;
+import org.example.myBatis.MyBatisSessionManager;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+
         System.out.println( "Hello World!" );
+
+       
     }
 }
