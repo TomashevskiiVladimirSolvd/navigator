923e31a9baae37abadfe19e10b541ffaff532971
chiemeliecode <ezeokeke.c@northeastern.edu>
2023-07-03 04:45:38 -0400

    Updated git ignore

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -19,6 +19,38 @@ public class App
 
         System.out.println( "Hello World!" );
 
-       
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        Connection connection = connectionPool.getConnectionFromPool();
+
+        try (Statement statement = connection.createStatement()) {
+            // Execute a sample query to test the connection
+            String query = "SELECT 1";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            if (resultSet.next()) {
+                int result = resultSet.getInt(1);
+                System.out.println("Database connection successful. Result: " + result);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            connectionPool.releaseConnectionToPool(connection);
+        }
+
+        // Initialize MyBatis
+        MyBatisInitializer.initialize();
+
+        // Get a SqlSession
+        SqlSession sqlSession = MyBatisSessionManager.getSqlSession();
+
+        try {
+            // Execute a sample query to test the connection
+            int count = sqlSession.selectOne("your.mapper.statement");
+
+            System.out.println("Database connection successful. Result: " + count);
+        } finally {
+            // Close the SqlSession
+            sqlSession.close();
+        }
     }
 }
