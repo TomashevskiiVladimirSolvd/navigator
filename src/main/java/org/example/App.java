a2875317775ab7aada52a18c7d0fe001a80d539e
James Remington Orozco Newton <JamesRONewton@YuanqingdeMacBook-Pro.local>
2023-07-06 13:42:33 -0700

    added random points generator

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,11 +11,20 @@ import org.apache.ibatis.session.SqlSession;
 import org.example.configurations.ConnectionPool;
 import org.example.myBatis.MyBatisInitializer;
 import org.example.myBatis.MyBatisSessionManager;
+import org.example.model.Point;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
     private static final Logger logger = Logger.getLogger("GLOBAL");
     public static void main( String[] args ) {
         PropertyConfigurator.configure("src/main/resources/log4j.properties");
-       
+        RandomPointsGenerator randomPointsGenerator = new RandomPointsGenerator(0,20,0,20, 5);
+        List<Point> points = Stream.generate(randomPointsGenerator::createRandomPoint)
+                .limit(randomPointsGenerator.getNumPoints())
+                .collect(Collectors.toList());
+        System.out.println(points);
     }
 }
