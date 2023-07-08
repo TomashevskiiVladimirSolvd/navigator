5378abe063cf72a1dd5f13363672c729bd2c87ff
chiemeliecode <ezeokeke.c@northeastern.edu>
2023-07-07 20:17:02 -0400

    Updated version of our work

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -23,51 +23,13 @@ import java.util.stream.Stream;
 public class App {
     private static final Logger logger = Logger.getLogger("GLOBAL");
     public static void main( String[] args ) {
-<<<<<<< HEAD
 
-        System.out.println( "Hello World!" );
-        ConnectionPool connectionPool = ConnectionPool.getInstance();
-        Connection connection = connectionPool.getConnectionFromPool();
-
-        try (Statement statement = connection.createStatement()) {
-            // Execute a sample query to test the connection
-            String query = "SELECT 1";
-            ResultSet resultSet = statement.executeQuery(query);
-
-            if (resultSet.next()) {
-                int result = resultSet.getInt(1);
-                System.out.println("Database connection successful. Result: " + result);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            connectionPool.releaseConnectionToPool(connection);
-        }
-
-        // Initialize MyBatis
-        MyBatisInitializer.initialize();
-
-        // Get a SqlSession
-        SqlSession sqlSession = MyBatisSessionManager.getSqlSession();
-
-        try {
-            // Execute a sample query to test the connection
-            int count = sqlSession.selectOne("your.mapper.statement");
-
-            System.out.println("Database connection successful. Result: " + count);
-        } finally {
-            // Close the SqlSession
-            sqlSession.close();
-        }
-
-
-=======
         PropertyConfigurator.configure("src/main/resources/log4j.properties");
         RandomPointsGenerator randomPointsGenerator = new RandomPointsGenerator(0,20,0,20, 5);
         List<Point> points = Stream.generate(randomPointsGenerator::createRandomPoint)
                 .limit(randomPointsGenerator.getNumPoints())
                 .collect(Collectors.toList());
         System.out.println(points);
->>>>>>> origin/main
+
     }
 }
